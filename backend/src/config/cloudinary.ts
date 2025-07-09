f301eb8940268777852e37ef8994ffb99e93fb50
Yash Puniwala <yashpuniwala@gmail.com>
2025-07-10 00:44:01 +0530

    added cloudinaru keyd

--- backend/src/config/cloudinary.ts
+++ backend/src/config/cloudinary.ts
@@ -3,9 +3,9 @@ import dotenv from "dotenv"
 dotenv.config({})
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "dv0yimv4z",
-    api_key: process.env.CLOUDINARY_API_KEY || "275175111542186",
-    api_secret: process.env.CLOUDINARY_SECRET_KEY || "JfvERs9TZ8_QRMihGZMz5ME1iyA"
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "dn982bksw",
+    api_key: process.env.CLOUDINARY_API_KEY || "213883857442458",
+    api_secret: process.env.CLOUDINARY_SECRET_KEY || "cMWyH4KoFqGsk2tI8NDI0mfeFQQ"
 })
 
 export const uploadMedia = async (file: string): Promise<UploadApiResponse | null> => {
